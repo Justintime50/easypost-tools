5e3687b540210c2172fb31b315cc4d0d5beb55b6
Justintime50 <jhammond@easypost.com>
2020-09-12 14:33:46 -0600

    Added C# create-webhook

---
+++ src/csharp/create-webhook/createWebhook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using EasyPost;
+using Newtonsoft.Json;
+
+namespace create_webhook
+{
+    class createWebhook
+    {
+        static void Main(string[] args)
+        {
+            EasyPost.ClientManager.SetCurrent(Environment.GetEnvironmentVariable("EASYPOST_API_KEY"));
+
+            Webhook webhook = Webhook.Create(
+                new Dictionary<string, object>() {
+                    { "url", "https://www.foobar.com" }
+                }
+            );
+            Console.WriteLine(JsonConvert.SerializeObject(webhook, Formatting.Indented));
+        }
+    }
+}
