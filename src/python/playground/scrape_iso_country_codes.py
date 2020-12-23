01712f4b8c273c1e25a99dc756e13d60354eafb6
Justintime50 <jhammond@easypost.com>
2020-12-23 15:49:19 -0700

    Fixing ISO country scraper

--- src/python/playground/scrape_iso_country_codes.py
+++ src/python/playground/scrape_iso_country_codes.py
@@ -1,12 +1,14 @@
 import sys
 import requests
+import json
 from bs4 import BeautifulSoup
 
 # Scrape the ISO Country Codes @ https://en.wikipedia.org/wiki/ISO_3166-1
-# USAGE: python3 scrape_iso_country_codes.py > iso_country_data.txt
+# USAGE: python3 scrape_iso_country_codes.py
 
 
 def main():
+    iso_data = []
     try:
         website = requests.get('https://en.wikipedia.org/wiki/ISO_3166-1')
         soup = BeautifulSoup(website.text, 'html.parser')
@@ -14,20 +16,21 @@ def main():
         country_table = tables[1]  # Scrape the 2nd table which is the ISO Country Codes
 
         for row in country_table.find_all('tr')[1:]:
-            # Here we could add the 5th column which is a link to each sudivision page
+            # TODO: Here we could add the 5th column which is a link to each sudivision page
             # Then, we could scrape the details of every country (eg: US states, etc)
             row_items = [td.text.strip() for td in row.find_all('td')]
             row_items = {
-                'country': row_items[0],  # TODO: Encode this string properly
-                'alpha_2_code': row_items[2],
-                'alpha_3_code': row_items[3],
+                'country': row_items[0],  # TODO: Encode this string properly (currently have to manually fix)
+                'alpha_2_code': row_items[1],
+                'alpha_3_code': row_items[2],
             }
-            print(row_items)
-            # Optionally write data to a file here
-            # with open('iso_country_codes.txt', 'a') as f:
-            #     f.write(str(row_items))
+            iso_data.append(row_items)
+            # print(row_items)
     except ValueError:
         sys.exit('Could not iterate the list provided.')
+
+    with open('files/iso_country_codes.json', 'w') as f:
+        f.write(str(json.dumps(iso_data, indent=4)))
 
 
 if __name__ == '__main__':
