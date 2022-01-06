3e0aa50a629fa7bae4e06c7ef71fe1398228bd22
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-01-05 19:08:02 -0700

    feat: add more examples

---
+++ src/examples/csharp/carrierAccounts/createCarrierAccount.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using EasyPost;
+
+namespace csharp
+{
+    class createCarrierAccount
+    {
+        static void Main(string[] args)
+        {
+            EasyPost.ClientManager.SetCurrent(Environment.GetEnvironmentVariable("EASYPOST_PROD_API_KEY"));
+
+            Dictionary<string, object> credentials = new Dictionary<string, object>() {
+                { "account_number", "A1A1A1" },
+                { "user_id", "USERID" },
+                { "password", "PASSWORD" },
+                { "access_license_number", "ALN" }
+            };
+
+            CarrierAccount account = CarrierAccount.Create(new Dictionary<string, object>() {
+                { "type", "UpsAccount" },
+                { "description", "description" },
+                { "credentials", credentials }
+            });
+
+            Console.WriteLine(JsonConvert.SerializeObject(account, Formatting.Indented));
+        }
+    }
+}
