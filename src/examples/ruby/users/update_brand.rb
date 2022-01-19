d665809561745e7c49c5d7016a8dfbec2449b42d
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-01-19 09:59:06 -0700

    fix: ruby lint

--- src/examples/ruby/users/update_brand.rb
+++ src/examples/ruby/users/update_brand.rb
@@ -10,14 +10,14 @@ EasyPost.api_key = ENV['EASYPOST_PROD_API_KEY']
 user = EasyPost::User.retrieve_me
 
 brand = user.update_brand(
-  color: "#303F9F",
+  color: '#303F9F',
   # logo: "data:image/png;base64,iVBOR...",
-  logo_href: "https://www.easypost.com",
-  name: "My Account Name",
+  logo_href: 'https://www.easypost.com',
+  name: 'My Account Name',
   ad: nil,
   ad_href: nil,
-  background_color: "#ffffff",
-  theme: "theme1"
+  background_color: '#ffffff',
+  theme: 'theme1'
 )
 
 ap brand
