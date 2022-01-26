8bea76dda0c60428acd366c8d004f355089d8af2
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-01-25 17:03:16 -0700

    feat: adds php api key retrieval examples

--- src/examples/php/users/retrieve-all-api-keys.php
+++ src/examples/php/users/retrieve-all-api-keys.php
@@ -12,11 +12,10 @@ $dotenv->load();
 EasyPost::setApiKey($_ENV['EASYPOST_PROD_API_KEY']);
 
 try {
-    // Pass the ID of a child user
-    $user = User::retrieve('user_123...');
-    $api_keys = $user->api_keys();
+    $user = User::retrieve_me();
+    $api_keys = $user->all_api_keys();
 
-    print_r($api_keys);
+    echo $api_keys;
 } catch (Error $exception) {
     echo $exception;
 }
