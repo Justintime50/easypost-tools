57ef11318874d667d4e569e62f5c3e99aeefffe3
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-01-27 10:53:48 -0700

    feat: adds create/delete API key tools

---
+++ src/tools/python/create_api_key.py
@@ -0,0 +1,28 @@
+import base64
+import os
+
+import requests
+from dotenv import load_dotenv
+
+# Creates an API key for the authenticated user. Perfect for child users who do not have access to the dashboard
+# You must pass in the mode you want the API key created for. You must use your API key of the same mode to authenticate
+
+load_dotenv()
+API_KEY = os.getenv("EASYPOST_PROD_API_KEY")
+
+ascii_encoded_api_key = API_KEY.encode('ascii')
+base64_bytes = base64.b64encode(ascii_encoded_api_key)
+base64_decoded_api_key = base64_bytes.decode('ascii')
+
+headers = {
+    'Authorization': f'Basic {base64_decoded_api_key}:',
+}
+response = requests.post(
+    'https://api.easypost.com/v2/api_keys',
+    headers=headers,
+    json={
+        'mode': 'production',
+    },
+)
+
+print(response.json())
---
+++ src/tools/python/delete_api_key.py
@@ -0,0 +1,24 @@
+import base64
+import os
+
+import requests
+from dotenv import load_dotenv
+
+# Deletes an API key for the authenticated user. Perfect for child users who do not have access to the dashboard
+
+load_dotenv()
+API_KEY = os.getenv("EASYPOST_PROD_API_KEY")
+
+ascii_encoded_api_key = API_KEY.encode('ascii')
+base64_bytes = base64.b64encode(ascii_encoded_api_key)
+base64_decoded_api_key = base64_bytes.decode('ascii')
+
+headers = {
+    'Authorization': f'Basic {base64_decoded_api_key}:',
+}
+
+# NOTE: this endpoint does not return a response
+requests.delete(
+    'https://api.easypost.com/v2/api_keys/ak_123...',
+    headers=headers,
+)
