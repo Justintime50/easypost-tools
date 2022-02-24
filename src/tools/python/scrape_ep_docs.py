7d5508c3f7a96582299bfbedbdb438eb0fbeb866
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-02-24 15:32:33 -0700

    fix: output scraped ep docs as JSON, not txt

--- src/tools/python/scrape_ep_docs.py
+++ src/tools/python/scrape_ep_docs.py
@@ -1,28 +1,33 @@
+import json
 import os
 import sys
 
 import requests
 from bs4 import BeautifulSoup
 
-# Scrape the EasyPost docs @ https://www.easypost.com/docs/api
+# Scrape the EasyPost docs @ https://www.easypost.com/docs/api for predefined packages or service levels
 # USAGE: SECTION=predefined-packages venv/bin/python scrape_ep_docs.py
 # Section options: 'predefined-packages' OR 'service-levels'
 
 
-def main(self):
+SECTION = os.getenv('SECTION')
+
+
+def main():
     try:
         website = requests.get('https://www.easypost.com/docs/api')
         soup = BeautifulSoup(website.text, 'html.parser')
-        carriers = soup.find('article', {'class': os.getenv('SECTION')})
+        carriers = soup.find('article', {'class': SECTION})
 
         # Iterate over each carrier and their items
+        output = {}
         for carrier in carriers.find_all('div', {'class': 'tab-pane'}):
             items = [li.text.strip() for li in carrier.find_all('li')]
             name = carrier.find('img')['alt']
-            output = '* ' + name + '\n\t\n\t'.join(items) + '\n'
-            print(output)
-            with open('easypost_service_levels.txt', 'a') as log:
-                log.write(output)
+            output[name] = items
+
+        with open(f'easypost-{SECTION}.json', 'w') as filename:
+            json.dump(output, filename, indent=4, sort_keys=True)
     except ValueError:
         sys.exit("Could not iterate the list provided.")
 
