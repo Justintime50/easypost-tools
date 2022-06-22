2223c056cfb1583cc65a79058925b47b7e05ab80
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-06-22 15:08:57 -0600

    fix: curl examples

--- src/examples/curl/addresses/create-and-verify-address.sh
+++ src/examples/curl/addresses/create-and-verify-address.sh
@@ -1,16 +1,15 @@
-curl -X POST https://api.easypost.com/v2/addresses \
+curl -X POST https://api.easypost.com/v2/addresses/create_and_verify \
 -u $EASYPOST_API_KEY: \
 -H 'Content-Type: application/json' \
 -d '{
     "address": {
-        "street1": "417 MONTGOMERY ST",
-        "street2": "FLOOR 5",
-        "city": "SAN FRANCISCO",
+        "street1": "417 Montgomery Street",
+        "street2": "FL 5",
+        "city": "San Francisco",
         "state": "CA",
         "zip": "94104",
         "country": "US",
         "company": "EasyPost",
         "phone": "415-123-4567"
-    },
-    "verify_strict": ["delivery"]
+    }
 }'
--- src/examples/curl/addresses/verify-address-on-create.sh
+++ src/examples/curl/addresses/verify-address-on-create.sh
@@ -1,6 +1,6 @@
 curl -X POST https://api.easypost.com/v2/addresses \
 -u $EASYPOST_API_KEY: \
--H "Content-Type: application/json" \
+-H 'Content-Type: application/json' \
 -d '{
     "address": {
         "street1": "417 MONTGOMERY ST",
