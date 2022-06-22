d46c6d3d68f431ee5b2f23cf40370d50624c6c09
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-06-22 11:00:21 -0600

    feat: add comprehensive curl examples

--- src/examples/shell/scanforms/create-scanform.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. /Users/jhammond/git/easypost/easypost-tools/.env
-
-curl -X POST https://api.easypost.com/v2/scan_forms \
-    -u "$EASYPOST_TEST_API_KEY": \
-    -d 'shipments[0][id]=shp_123...' \
-    -d 'shipments[1][id]=shp_123...'
